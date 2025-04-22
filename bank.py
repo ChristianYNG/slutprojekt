548f32e9a624857159c62f5acaa3d42dc13dc249
Christian <121486932+ChristianYNG@users.noreply.github.com>
2025-04-22 10:21:46 +0200

    Började med att lägga till transfer funktion

--- bank.py
+++ bank.py
@@ -7,7 +7,7 @@ class Account: # Klass för att skapa konton
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount # Lägg till ett insättning
-            print(f"{amount} SEK successfully deposited")
+            print(f"{amount} SEK successfully deposited to account {account_number} ")
             self.history.append(f"deposit: {amount} SEK") # sparar insättningne i en lista
         else:
             print("Deposit must be larger than 0 SEK")
@@ -21,8 +21,19 @@ class Account: # Klass för att skapa konton
     def check_balance(self):
         print(f"Available Balance: {self.balance} SEK")
 
+    def transfer(self, transfer_amount):
+        print(f"{transfer_amount} succesfully transfered to account {4455}")
+        if 0 < transfer_amount <= self.balance:
+            self.balance -= transfer_amount
+            accounts[5454].balance += transfer_amount
+            self.history.append(f"Transfer: -{transfer_amount} SEK to account 5454") # Lägger till i history
+            accounts[5454].history.append(f"Transfer: +{transfer_amount} SEK from account {self.account_number}")
+            print(f"{transfer_amount} SEK successfully transferred to account 5454")
+        else:
+            print("Error transferring given amount")
 
-accounts = {} # En tom dict som håller alla kontonummer
+
+accounts = {5454: Account(5454)} # En dict som håller alla kontonummer, 
 
 while True:
     print("\n1. Sign In\n2. Exit")
@@ -47,18 +58,26 @@ while True:
 
             elif choice == "2" or choice.lower() == "deposit":
                 amount = float(input("Enter amount to deposit: "))
-                user_account.deposit(amount)
 
-            elif choice == "3" or choice.lower() == "withdraw":
+
+            elif choice == "3" or choice.lower() == "transfer":
+                transfer_amount = float(input("Enter amount to transfer to savings: "))
+
+
+            elif choice == "4" or choice.lower() == "withdraw":
                 amount = float(input("Enter amount to withdraw: "))
                 user_account.withdraw(amount)
 
-            elif choice == "4" or choice.lower() == "transaction history":
+            elif choice == "5" or choice.lower() == "transfer":
+                transfer_amount = float(input("Enter amount to transfer: "))
+                user_account.transfer(transfer_amount)
+
+            elif choice == "6" or choice.lower() == "transaction history":
                 print("Transaction History:")
                 for entry in user_account.history:
                     print(entry)
 
-            elif choice == "5" or choice.lower() == "sign out":
+            elif choice == "7" or choice.lower() == "sign out":
                 print("Signed out successfully!")
                 break  
 
